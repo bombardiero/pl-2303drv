b049779e26d65e773c22bc6dd8e9aa8cbbb3527c
Bomba <bomba@bomba.com>
2015-05-24 16:10:44 +0200

    3

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # pl-2303drv
+<br />
+http://stackoverflow.com/questions/5417732/howto-prepare-qtcreator-for-linux-driver-kernel-development
+<br />
--- src/modulek.c
+++ src/modulek.c
@@ -5,16 +5,15 @@
 #include <linux/usb.h>
 
 
-
 int init_module(void)
 {
-	printk(KERN_INFO "Hello world 1.\n");
+    printk(KERN_INFO "Hello world 1.\n");
     //struct urb *urb, gfp_t mem_flags
-   usb_submit_urb(NULL, 0);
-	/* 
-	 * A non 0 return means init_module failed; module can't be loaded. 
-	 */
-	return 0;
+    usb_submit_urb(NULL, 0);
+    /*
+     * A non 0 return means init_module failed; module can't be loaded.
+     */
+    return 0;
 }
 
 void cleanup_module(void)
